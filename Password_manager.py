b3ebb0eb430248ed908d93e305cb8b9e3dd89c86
Mwamtindi <mwamtindishaban@gmail.com>
2024-02-08 17:06:22 +0300

    Password manager

---
+++ Password_manager.py
@@ -0,0 +1,55 @@
+from cryptography.fernet import Fernet, InvalidToken
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)'''
+
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+key = load_key()
+fer = Fernet(key)
+
+
+def view():
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            try:
+                decrypted_pass = fer.decrypt(passw.encode()).decode()
+                print("User:", user, "| Password:", decrypted_pass)
+            except InvalidToken:
+                print("Invalid password data detected for user:", user)
+
+
+def add():
+    name = input('Account Name: ')
+    pwd = input("Password: ")
+
+    with open('passwords.txt', 'a') as f:
+        encrypted_pwd = fer.encrypt(pwd.encode())
+        f.write(name + "|" + encrypted_pwd.decode() + "\n")
+
+
+
+while True:
+    mode = input(
+        "Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower()
+    if mode == "q":
+        break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode.")
+        continue
